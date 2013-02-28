a898321b15315814dcdd5976ddb787464dbc94cc
chernikov <chernikov@localhost>
2013-02-28 14:20:33 +0200

    Add Lesson 7

---
+++ lesson7/LessonProject/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LessonProject.Global.Auth;
+using LessonProject.Mappers;
+using LessonProject.Model;
+using Ninject;
+
+namespace LessonProject.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        [Inject]
+        public IRepository Repository { get; set; }
+
+        [Inject]
+        public IMapper ModelMapper { get; set; }
+
+        [Inject]
+        public IAuthentication Auth { get; set; }
+
+        public User CurrentUser
+        {
+            get
+            {
+                return ((IUserProvider)Auth.CurrentUser.Identity).User;
+            }
+        }
+    }
+}
