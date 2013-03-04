0eed3674d41d12cfffaa41be51c1acfaaba1efbf
chernikov <chernikov@localhost>
2013-03-04 17:31:21 +0200

    Lesson 8

---
+++ lesson8/LessonProject/Controllers/BaseController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LessonProject.Global.Auth;
+using LessonProject.Mappers;
+using LessonProject.Model;
+using Ninject;
+
+namespace LessonProject.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        [Inject]
+        public IRepository Repository { get; set; }
+
+        [Inject]
+        public IMapper ModelMapper { get; set; }
+
+        [Inject]
+        public IAuthentication Auth { get; set; }
+
+        public User CurrentUser
+        {
+            get
+            {
+                return ((IUserProvider)Auth.CurrentUser.Identity).User;
+            }
+        }
+
+        protected static string ErrorPage = "~/Error";
+
+        protected static string NotFoundPage = "~/NotFoundPage";
+
+        protected static string LoginPage = "~/Login";
+
+        public RedirectResult RedirectToNotFoundPage
+        {
+            get
+            {
+                return Redirect(NotFoundPage);
+            }
+        }
+
+        public RedirectResult RedirectToLoginPage
+        {
+            get
+            {
+                return Redirect(LoginPage);
+            }
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+
+            filterContext.Result = Redirect(ErrorPage);
+        }
+    }
+}
---
+++ lesson8/LessonProject/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using LessonProject.Areas.Admin;
+using LessonProject.Areas.Default;
+
+namespace LessonProject
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var adminArea = new AdminAreaRegistration();
+            var adminAreaContext = new AreaRegistrationContext(adminArea.AreaName, RouteTable.Routes);
+            adminArea.RegisterArea(adminAreaContext);
+
+            var defaultArea = new DefaultAreaRegistration();
+            var defaultAreaContext = new AreaRegistrationContext(defaultArea.AreaName, RouteTable.Routes);
+            defaultArea.RegisterArea(defaultAreaContext);
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
