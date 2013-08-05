8d6b8dec9d808ab16b6335e316267f24aa5476bf
chernikov <chernikov@localhost>
2013-08-05 18:03:58 +0300

    Update Lesson 3

--- lesson3/LessonProject/App_Start/NinjectWebCommon.cs
+++ lesson3/LessonProject/App_Start/NinjectWebCommon.cs
@@ -1,16 +1,18 @@
-using System;
-using System.Configuration;
-using System.Web;
-using System.Web.Mvc;
-using LessonProject.Model;
-using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-using Ninject;
-using Ninject.Web.Common;
-
 [assembly: WebActivator.PreApplicationStartMethod(typeof(LessonProject.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(LessonProject.App_Start.NinjectWebCommon), "Stop")]
+
 namespace LessonProject.App_Start
 {
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using LessonProject.Model;
+    using System.Configuration;
+
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
@@ -57,6 +59,4 @@ namespace LessonProject.App_Start
             kernel.Bind<IRepository>().To<SqlRepository>().InRequestScope();
         }        
     }
-
-    
 }
